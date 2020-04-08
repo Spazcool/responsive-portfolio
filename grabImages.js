17e382eece02a1a53c1f66620e56c97da7e2ba1e
Spazcool <douglas.wrightiii@gmail.com>
2020-04-08 09:01:14 -0400

    WIP: broke of styling to separate CSS file, added custom JS for pulling images and creating cards

---
+++ grabImages.js
@@ -0,0 +1,68 @@
+function loadImgs() {
+    let baseUrl = 'https://api.nasa.gov/planetary/apod?'
+    let date;
+    let key = 'evffvWkvE20YwQYQqknagdjGhb0FZjthYH0ETOTo';
+    let requestUrl;
+
+    for( let i = 0; i < 8; i++){
+        // RANDOMIZE DATE VALUE, AS A PROXY FOR RANDOMIZING THE IMAGES
+        date = randomDate(new Date(2012, 0, 1), new Date());
+        requestUrl = baseUrl + 'api_key=' + key + '&date=' + date;
+        fetch(requestUrl)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                createNodes(data, i);
+            })
+    }
+}
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString().slice(0,10);
+}
+
+// THE REASON FOR REACT/ANGULAR ⬇
+function createNodes(data, index){
+    // CREATE NODES
+    let div1 = document.createElement("DIV");
+    let div2 = document.createElement("DIV");
+    let img = document.createElement("IMG");
+    let title = document.createElement("H5");
+    let para = document.createElement("P");
+
+    // ADD NECESSARY ATTRIBUTES
+    div1.className = "card";
+    div1.id = "div" + index;
+    div2.className = "card-body";
+    div2.id = "div" + index;
+    img.className = "card-img-top";
+    img.id = "img";
+    para.innerHTML = data.explanation;
+    img.src = data.url;
+    title.innerHTML = data.title
+
+    // ADD NODES TO DOCUMENT
+    document.getElementById("colleciton").appendChild(div1);
+    document.getElementById(div1.id).appendChild(img);
+    document.getElementById(div1.id).appendChild(div2);
+    document.getElementById(div2.id).appendChild(title);
+    document.getElementById(div2.id).appendChild(para);
+}
+
+// todo: nodes created above seem to have some random padding, why?
+// ---
+// https://api.nasa.gov/
+// https://api.nasa.gov/planetary/apod?api_key=evffvWkvE20YwQYQqknagdjGhb0FZjthYH0ETOTo&date=2020-01-01
+
+// {
+//     "date": "2020-01-01",
+//     "explanation": "Why is Betelgeuse fading?  No one knows.  Betelgeuse, one of the brightest and most recognized stars in the night sky, is only half as bright as it used to be only five months ago.  Such variability is likely just  normal behavior for this famously variable supergiant, but the recent dimming has rekindled discussion on how long it may be before Betelgeuse does go supernova.  Known for its red color, Betelgeuse is one of the few stars to be resolved by modern telescopes, although only barely.  The featured artist's illustration imagines how Betelgeuse might look up close. Betelgeuse is thought to have a complex and tumultuous surface that frequently throws impressive flares.  Were it to replace the Sun (not recommended), its surface would extend out near the orbit of Jupiter, while gas plumes would bubble out past Neptune.  Since Betelgeuse is about 700 light years away, its eventual supernova will not endanger life on Earth even though its brightness may rival that of a full Moon.  Astronomers -- both amateur and professional -- will surely continue to monitor Betelgeuse as this new decade unfolds.    Free Presentation: APOD Editor to show best astronomy images of 2019 -- and the decade -- in NYC on January 3",
+//     "hdurl": "https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_2662.jpg",
+//     "media_type": "image",
+//     "service_version": "v1",
+//     "title": "Betelgeuse Imagined",
+//     "url": "https://apod.nasa.gov/apod/image/2001/BetelgeuseImagined_EsoCalcada_960.jpg"
+// }
+
+
